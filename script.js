1ea8936afe3f285ec6bc2c3c75f8d2a0b5ba493a
Edgar mamikonian <89902233+sSpectralls@users.noreply.github.com>
2022-04-25 09:49:10 +0000

    -bug fixes -timer - work in progress mobile

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let counter = 0;
 let firstSelection = "";
 let secondSelection = "";
+let time = 0;
+let TimeStarter = 0;
+let seconds = 0;
+let Player = "player1";
+let correctCardCounter = 0;
+let Timer = document.getElementById("timer");
 const card = document.querySelectorAll(".card");
 const score1 = document.querySelector(".score1 span");
 const score2 = document.querySelector(".score2 span");
 const Reset = document.getElementById("reset");
 const cards = document.querySelectorAll(".cards .card");
-let Player = "player1";
+const AllCards = document.getElementsByClassName("cards");
 
 
 shuffleImage()
@@ -22,6 +28,33 @@ function shuffleImage(){
     })
 }
 
+function startTime () {
+  time = setInterval ( function (){
+    seconds++;
+    Timer.innerHTML = 'Timer:'+ seconds + 's';                //Timer functie, na 1 seconde verandert de tekst met +1 erbij
+  }, 1000); 
+  startTime = function() {};                  //zorgt dat het niet herhaad, zodat het niet steeds +2 wordt dan +3 
+}
+
+function stopTime () {
+  clearInterval (time);                  //functie zodat de tijd stopt 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Main game//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 cards.forEach((card) => {
     card.addEventListener("click", () => {
@@ -29,11 +62,16 @@ cards.forEach((card) => {
         if (counter === 0) {
           firstSelection = card.getAttribute("meme");
           counter++;
+          TimeStarter = 1;                                  //begint de timer als TimeStarter 1 is, het wordt 1 als de eerste kaart gekozen is. Dat wordt gecheckt met counter.
+          if (TimeStarter === 1) {
+            startTime();
+          }
           
         } else {
           secondSelection = card.getAttribute("meme");
           counter = 0;
-    
+
+
           if (firstSelection === secondSelection) {
             const correctCards = document.querySelectorAll(".card[meme='" + firstSelection + "']");
              if (Player == "player1") {
@@ -44,14 +82,27 @@ cards.forEach((card) => {
                score2.innerHTML = parseInt(score2.innerHTML) + 1
                 if (score2.innerHTML >= 8)
                   score2.innerHTML = 8;
-             }                            //update the score met +1 als eerste selectie hetzelfde is als 2e selectie, de maximale score is 8.
+             }                            
+            
+            //update the score met +1 als eerste selectie hetzelfde is als 2e selectie, de maximale score is 8.
             correctCards[0].classList.add("checked");
             correctCards[0].classList.remove("clicked");
             correctCards[1].classList.add("checked");                        //haalt de class "clicked" weg want je bent er klaar me.
             correctCards[1].classList.remove("clicked");
-              
-    
+            correctCardCounter++;
             
+            if (correctCardCounter == 8) {
+              stopTime();                                  //de tijd stopt als er 8 correcte paren zijn
+            }
+            if (correctCardCounter == 8 && score1.innerHTML > score2.innerHTML) {
+              alert("Congratulations, player 1 won");    
+            }
+            if (correctCardCounter == 8 && score1.innerHTML < score2.innerHTML) {
+              alert("Congratulations, player 2 won");  
+            } 
+            if (correctCardCounter == 8 && score1.innerHTML == score2.innerHTML) {
+              alert("It's a draw!!")
+            }
           } else {
               const incorrectCards = document.querySelectorAll(".card.clicked");
       
@@ -74,6 +125,5 @@ cards.forEach((card) => {
         }
       });
 });
-
 
 
